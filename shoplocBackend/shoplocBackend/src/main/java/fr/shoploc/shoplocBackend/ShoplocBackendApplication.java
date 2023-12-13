0d41f258266adea2566e71c5f0d5eb92f84d837f
camille <camillermpoirier@gmail.com>
2023-12-13 18:11:00 +0100

    initial commit for shoploc Backend

---
+++ shoplocBackend/shoplocBackend/src/main/java/fr/shoploc/shoplocBackend/ShoplocBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.shoploc.shoplocBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplocBackendApplication.class, args);
+	}
+
+}
