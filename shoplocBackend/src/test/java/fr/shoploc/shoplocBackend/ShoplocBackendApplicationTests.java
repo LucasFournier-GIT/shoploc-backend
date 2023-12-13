dad39373d62147ebf8604e2238be636f02f0a148
camille <camillermpoirier@gmail.com>
2023-12-13 18:17:36 +0100

    fix project structure

---
+++ shoplocBackend/src/test/java/fr/shoploc/shoplocBackend/ShoplocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.shoploc.shoplocBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
