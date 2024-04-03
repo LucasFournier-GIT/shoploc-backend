02dd9a4de4553735c191007c90c9ed6e20987ef2
Lucas Fournier <lucas.fournier@adeo.com>
2024-04-04 01:39:01 +0200

    feat: refacto and add endpoint to get user

--- src/main/java/fr/shoploc/shoplocBackend/usermanager/user/UserController.java
+++ src/main/java/fr/shoploc/shoplocBackend/usermanager/user/UserController.java
@@ -1,14 +1,11 @@
 package fr.shoploc.shoplocBackend.usermanager.user;
 
-import fr.shoploc.shoplocBackend.dto.ShopDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
