77ae084b1ad10c7eed86ad6d45712bd83b560d36
Lucas Fournier <lucas.fournier@adeo.com>
2024-04-04 01:38:29 +0200

    feat(SCRUM-91): fix tests

---
+++ src/main/java/fr/shoploc/shoplocBackend/usermanager/user/UserController.java
@@ -0,0 +1,33 @@
+package fr.shoploc.shoplocBackend.usermanager.user;
+
+import fr.shoploc.shoplocBackend.dto.ShopDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getUser(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.substring(7);
+        try {
+            return ResponseEntity.ok().body(userService.getUser(token));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}
